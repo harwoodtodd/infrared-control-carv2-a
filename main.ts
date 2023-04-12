18596fcd5c880a73335aa75a9116036d16cda5d1
harwoodtodd <128326293+harwoodtodd@users.noreply.github.com>
2023-04-12 16:20:53 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,33 +11,6 @@ makerbit.onIrButton(IrButton.Light, IrButtonAction.Pressed, function () {
     mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.White)
 })
 makerbit.onIrButton(IrButton.NUM9, IrButtonAction.Pressed, function () {
-    basic.showNumber(9)
-})
-makerbit.onIrButton(IrButton.Right, IrButtonAction.Pressed, function () {
-    mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Right, 100)
-})
-makerbit.onIrButton(IrButton.Left, IrButtonAction.Pressed, function () {
-    mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Left, 100)
-})
-makerbit.onIrButton(IrButton.Minus, IrButtonAction.Pressed, function () {
-    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Blue)
-})
-makerbit.onIrButton(IrButton.Down, IrButtonAction.Pressed, function () {
-    mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Back, 100)
-})
-makerbit.onIrButton(IrButton.NUM2, IrButtonAction.Pressed, function () {
-    basic.showNumber(2)
-})
-makerbit.onIrButton(IrButton.Up, IrButtonAction.Pressed, function () {
-    mbit_Robot.CarCtrlSpeed2(mbit_Robot.CarState.Car_Run, 100, 50)
-})
-makerbit.onIrButton(IrButton.Power, IrButtonAction.Pressed, function () {
-    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Red)
-})
-makerbit.onIrButton(IrButton.NUM0, IrButtonAction.Pressed, function () {
-    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Green)
-})
-makerbit.onIrButton(IrButton.BEEP, IrButtonAction.Pressed, function () {
     mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Stop)
     basic.showLeds(`
         # . . . #
@@ -47,8 +20,33 @@ makerbit.onIrButton(IrButton.BEEP, IrButtonAction.Pressed, function () {
         # . . . #
         `)
 })
+makerbit.onIrButton(IrButton.Right, IrButtonAction.Pressed, function () {
+	
+})
+makerbit.onIrButton(IrButton.Left, IrButtonAction.Pressed, function () {
+	
+})
+makerbit.onIrButton(IrButton.Minus, IrButtonAction.Pressed, function () {
+    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Blue)
+})
+makerbit.onIrButton(IrButton.Down, IrButtonAction.Pressed, function () {
+	
+})
+makerbit.onIrButton(IrButton.NUM2, IrButtonAction.Pressed, function () {
+    basic.showNumber(2)
+})
+makerbit.onIrButton(IrButton.Up, IrButtonAction.Pressed, function () {
+    mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, 0)
+    mbit_Robot.CarCtrlSpeed2(mbit_Robot.CarState.Car_Run, 0, 100)
+})
+makerbit.onIrButton(IrButton.Power, IrButtonAction.Pressed, function () {
+    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Red)
+})
+makerbit.onIrButton(IrButton.NUM0, IrButtonAction.Pressed, function () {
+    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Green)
+})
 makerbit.onIrButton(IrButton.TRight, IrButtonAction.Pressed, function () {
-    mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_SpinRight, 100)
+	
 })
 makerbit.onIrButton(IrButton.Plus, IrButtonAction.Pressed, function () {
     mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Red)
